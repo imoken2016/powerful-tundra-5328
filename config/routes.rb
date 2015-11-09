10bd5cec7f076cf06fe6fc561dbc07b44364e0dc
imototakeshi <imoto@niocle.com>
2015-11-09 14:14:39 +0000

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :user
   root to: "top#index" 
   resources :blogs
   get 'contacts' => 'contacts#index'              # 入力画面
   post 'contacts' => 'contacts#index'              # 入力画面
   post 'contacts/confirm' => 'contacts#confirm'   # 確認画面
   post 'contacts/thanks' => 'contacts#thanks'     # 送信完了画面
+  #devise_for :users, :controllers => {
+  #:sessions      => "users/sessions",
+  #:registrations => "users/registrations",
+  #:passwords     => "users/passwords"
+  #}
+  
 end
