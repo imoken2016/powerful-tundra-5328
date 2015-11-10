ad5862c72bbd9280363ddceb5356a44c8c8659f4
imototakeshi <imoto@niocle.com>
2015-11-10 14:36:41 +0000

    メール送信

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_mailer_host
   
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:account_update) << :name
     end
+  
+  private
+    def set_mailer_host
+      ActionMailer::Base.default_url_options[:host] = request.host_with_port
+    end
+  
 end
