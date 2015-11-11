677703ebef424848326ad58f2e0567aa2cc48670
imototakeshi <imoto@niocle.com>
2015-11-11 06:23:39 +0000

    Bootstrap追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :user
+  resources :user, only: [:index, :show]
   root to: "top#index" 
   resources :blogs
   get 'contacts' => 'contacts#index'              # 入力画面
   post 'contacts' => 'contacts#index'              # 入力画面
   post 'contacts/confirm' => 'contacts#confirm'   # 確認画面
   post 'contacts/thanks' => 'contacts#thanks'     # 送信完了画面
-  #devise_for :users, :controllers => {
-  #:sessions      => "users/sessions",
-  #:registrations => "users/registrations",
-  #:passwords     => "users/passwords"
-  #}
   
 end
