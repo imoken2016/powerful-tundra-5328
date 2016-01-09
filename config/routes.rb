67031857d2ed658048ae078634b4b67cefc02b6f
imototakeshi <imoto@niocle.com>
2016-01-09 02:01:44 +0000

    てすとについて

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'about/company_overview'
+
+  get 'about/company_overview'
+
   get 'users' => 'users#index'
   get 'users/show'
   devise_for :user, controllers: { 
@@ -15,5 +19,7 @@ Rails.application.routes.draw do
   post 'contacts' => 'contacts#index'              # 入力画面
   post 'contacts/confirm' => 'contacts#confirm'   # 確認画面
   post 'contacts/thanks' => 'contacts#thanks'     # 送信完了画面
+  
+  get 'about' => 'about#company_overview'
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#blog = Blog.create([{ title: 'できた！', content: '本文を入れるよ',user_id: 28 }])
+#User.create(id: 28, blog_id: blog.first)
+#user = User.create(id: 28)
+#Blog.create([{ title: 'できた！', content: '本文を入れるよ',user_id: user.id }])
+#Title.create(:name => '観察日記', :sales_date => '2011-11-14', :price => 1000)
+
+2.times do |no|
+  user = User.create(name: "名無し太朗#{no}号", email: "hogehoge#{no}@example.com", password: '12345678', password_confirmation: '12345678', confirmed_at: Time.now,uid: no, provider: no)
+  Blog.create(title: "できたよ！#{no}", content: "本文を入れるよ", user_id: user.id )
+end
